b0b5a5e7e0e9683d6ee0887625793ece03271e3c
john2796 <jbmiranda22796@gmail.com>
2019-02-22 11:23:28 -0800

    added more seed and deployed to pg

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,24 @@
 // Update with your config settings.
+const pg = require("pg");
+pg.defaults.ssl = true;
+const pg_key =
+  "postgres://lyoenwrkcepwag:06cfc1eda1e0762eb8cf21f7e37e3a6f3dd96e853c85c96c5609f15624db5980@ec2-23-21-130-182.compute-1.amazonaws.com:5432/dcin2tvajg4u50";
 
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/db.sqlite3"
-    },
+    client: "pg",
+    connection: pg_key,
     useNullAsDefault: true,
     migrations: {
+      tableName: "knex_migrations",
       directory: "./data/migrations"
     },
     seeds: {
       directory: "./data/seeds"
+    },
+    pool: {
+      min: 2,
+      max: 10
     }
   }
 
