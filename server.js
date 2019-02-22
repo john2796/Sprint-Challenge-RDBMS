ea1c64ba341da304da37a9843dff7b9d20964839
john2796 <jbmiranda22796@gmail.com>
2019-02-22 10:13:28 -0800

    adding update

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`
   --------------------------------------------------------------------
-                        Server running on port ${port}
+                     Server running on port ${port}
   --------------------------------------------------------------------
   `);
 });
